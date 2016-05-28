9c4063c3115eac7b94144972809f6f2416d124b5
MacRat <m@crat.jp>
2016-05-28 21:45:49 +0900

    Made graphics

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,6 +1,10 @@
 #include "ofApp.h"
 
 
+#define CIRCLE_SIZE	128.0
+#define ONE_NOTE	3.0
+
+
 void ofApp::setup() {
 }
 
@@ -10,20 +14,45 @@ void ofApp::update() {
 
 
 void ofApp::draw() {
-}
+	ofBackgroundGradient(ofColor::gray, ofColor::black);
 
+	ofEnableBlendMode(OF_BLENDMODE_ADD);
+	ofSetColor(255, 255, 255, 64);
+	for(const ofPoint p: notes) {
+		ofDrawCircle(
+			p.x * ofGetWidth(),
+			p.y * ofGetHeight(),
+			min(ofGetWidth(), ofGetHeight())/CIRCLE_SIZE
+		);
+	}
+	ofDisableBlendMode();
 
-void ofApp::mouseMoved(int x, int y ) {
+	ofSetColor(255, 255, 255, 128);
+	ofDrawLine(
+		ofGetElapsedTimeMillis()%((int)ONE_NOTE*1000)/ONE_NOTE/1000.0 * ofGetWidth(),
+		0,
+		ofGetElapsedTimeMillis()%((int)ONE_NOTE*1000)/ONE_NOTE/1000.0 * ofGetWidth(),
+		ofGetHeight()
+	);
 }
 
 
 void ofApp::mouseDragged(int x, int y, int button) {
+	const ofPoint mouse = ofPoint((float)x/ofGetWidth(), (float)y/ofGetHeight());
+
+	if(button == 0){
+		notes.push_back(mouse);
+	}else if(button == 2){
+		ofRemove(notes, [mouse](const ofPoint x){ return mouse.distance(x) < 2.0/CIRCLE_SIZE; });
+	}
 }
 
 
 void ofApp::mousePressed(int x, int y, int button) {
+	mouseDragged(x, y, button);
 }
 
 
 void ofApp::mouseReleased(int x, int y, int button) {
+	mouseDragged(x, y, button);
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,19 +1,23 @@
 #ifndef __OF_APP_H__
 #define __OF_APP_H__
 
+#include <memory>
+
 #include <ofMain.h>
 
 
-class ofApp : public ofBaseApp{
-public:
-	void setup();
-	void update();
-	void draw();
+class ofApp : public ofBaseApp {
+private:
+	std::vector< ofPoint > notes;
 
-	void mouseMoved(int x, int y );
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
+public:
+	void setup() override;
+	void update() override;
+	void draw() override;
+
+	void mouseDragged(const int x, const int y, const int button) override;
+	void mousePressed(const int x, const int y, const int button) override;
+	void mouseReleased(const int x, const int y, const int button) override;
 };
 
 
