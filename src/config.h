259cfcdc0472beb00b5e6856770b90f509581c07
MacRat <m@crat.jp>
2016-05-29 16:37:44 +0900

    Changed volume

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 #define CIRCLE_SIZE	128.0L
 #define EFFECT_TTL	300.0
 #define EFFECT_SIZE	3.0
-#define VOLUME		0.05L
+#define VOLUME		0.01L
 #define SAMPLE_RATE	44100
 #define MIN_FREQ	60.0L
 #define MAX_FREQ	3000.0L
